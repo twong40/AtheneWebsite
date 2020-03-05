744c587fec70ba0b2fc3ed899d26ec9646d10d28
thomaswong1000@gmail.com <twong40@ucsc.edu>
2020-03-04 17:05:57 -0800

    added basic homepage, testing contact requets

--- website/src/components/Home.js
+++ website/src/components/Home.js
@@ -21,12 +21,113 @@ class Home extends Component {
         </p>
         <br />
         <p className="display-3">At a Quick Glance:</p>
-        <div className="container m-3 p-3">
+        <div className="container box-container align-self-center p-4">
           <div className="row">
-            <div className="col-sm-4">
-              <span className="display-4 pt-3">Techincal Skills</span>
+            <div className="col-6 col-md-3 box-tag-1">
+              <br />
+              <br />
+              <br />
+              <br />
+              <span className="pr-3 mr-3 text-left text-wrap">
+                <h1>Techincal Skills</h1>
+              </span>
             </div>
-            <div className="col-sm-8">One of three columns</div>
+            <div className="col-6 col-md-3 box-data-1">
+              <ul className="p-3 pl-5">
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+              </ul>
+            </div>
+            <div className="col-6 col-md-3 box-data-1">
+              <ul className="p-3 pl-5">
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+              </ul>
+            </div>
+            <div className="col-6 col-md-3 box-data-1">
+              Learning:
+              <ul className="pr-3 pl-5 pd-3">
+                <li>List Item</li>
+                <li>List Item</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="container box-container align-self-center p-4">
+          <div className="row">
+            <div className="col-6 col-md-3 box-tag-1">
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <span className="pr-3 mr-3 text-left text-wrap">
+                <h1>Soft Skills</h1>
+              </span>
+            </div>
+            <div className="col-6 col-md-3 box-data-1">
+              <ul className="p-3 pl-5">
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+              </ul>
+            </div>
+            <div className="col-6 col-md-3 box-data-1">
+              <ul className="p-3 pl-5">
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+              </ul>
+            </div>
+            <div className="col-6 col-md-3 box-data-1">
+              <ul className="p-3 pl-5">
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+                <li>List Item</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="container box-container align-self-center p-4">
+          <div className="row">
+            <div className="col-6 col-md-3 box-tag-1">
+              <br />
+              <br />
+              <br />
+              <br />
+              <span className="pr-3 mr-3 text-left text-wrap">
+                <h1>Current Projects</h1>
+              </span>
+            </div>
+            <div className="col box-data-1">
+              <ul>
+                <br />
+                <br />
+                <li className="text-wrap pr-4 pt-3 pb-3">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Nec feugiat in fermentum posuere urna. Nec feugiat in
+                  fermentum posuere urna nec.
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
