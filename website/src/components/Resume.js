ec08b77dbcfd628c801b689b55c7fbf33547f5dd
thomaswong1000@gmail.com <twong40@ucsc.edu>
2020-03-07 17:43:29 -0800

    Added basic resume page

--- website/src/components/Resume.js
+++ website/src/components/Resume.js
@@ -1,14 +1,62 @@
 import React, { Component } from "react";
-
+import resume from "../assets/resume/ResumeDisplay.png";
+import "../App.css";
 class Resume extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { width: 0, height: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
   }
   render() {
+    const smallResize = this.state.width < 450 && this.state.height < 850;
     return (
       <div className="container">
-        <h1>Resume</h1>
+        <hr />
+        {smallResize ? (
+          <img src={resume} className="img-fluid p-3" alt="Resume"></img>
+        ) : (
+          <div className="container w-75 p-3">
+            <img src={resume} className="img-fluid" alt="Resume"></img>
+          </div>
+        )}
+        <div className="text-center">
+          <span className="p-3 m-3">
+            <a
+              href="../assets/resume/AtheneYip_Resume.pdf"
+              className="btn btn-primary shadow"
+              download
+            >
+              Download Resume
+            </a>
+          </span>
+          {smallResize && (
+            <div className="container">
+              <br />
+            </div>
+          )}
+          <span className="p-3 m-3">
+            <a
+              href="../assets/resume/AtheneYip_Resume_bw.pdf"
+              className="btn btn-secondary shadow"
+              download
+            >
+              Black & White Version
+            </a>
+          </span>
+        </div>
       </div>
     );
   }
